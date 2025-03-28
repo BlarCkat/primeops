99501c6d01749398ff9526478e0d6256ec292fd1
John St. Aïmond Banson <articwolf177@gmail.com>
2025-03-28 23:09:52 +0100

    base:start

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+const HomeView = () => {
+  return ( 
+    <>
+    <main className="h-screen w-screen flex flex-col gap-2 items-center justify-start p-[5vw]">
+      <span className="font-semibold text-[64px] tracking-tighter">Primeops Hackathon, Day 1</span>
+      <p>A financial literacy platform to help educate the young adult about money and how to invest, save, manage and grow their finances.</p>
+    </main>
+    </>
+   );
+}
+ 
+export default HomeView;
