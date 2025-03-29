80734b5375c45945f3b1de34616320281fc9e75a
John St. Aïmond Banson <articwolf177@gmail.com>
2025-03-29 11:08:09 +0100

    edit:AuthContext

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,4 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
+// import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = "https://utmjwoylfmatxxxrqlrn.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0bWp3b3lsZm1hdHh4eHJxbHJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyMDQ1NjgsImV4cCI6MjA1ODc4MDU2OH0.KL4K0H7S7G18Vb8l0zZB9ziRKaYe7-6ckyD55gau7u0";
@@ -6,9 +7,9 @@ const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 // const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 // const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-if (!supabaseURL || !supabaseKey) {
-    throw new Error('Supabase URL and Anon Key are required.');
-}
-console.log(supabaseKey, supabaseURL);
+// if (!supabaseURL || !supabaseKey) {
+//     throw new Error('Supabase URL and Anon Key are required.');
+// }
 
-export const supabase = createClient(supabaseURL, supabaseKey);
+// export const supabase = createClient(supabaseURL, supabaseKey);
+export const supabase = createBrowserClient(supabaseURL!, supabaseKey!)
