01cb20ca4324ed3ab675df9141ff023b48f30d60
John St. Aïmond Banson <articwolf177@gmail.com>
2025-03-29 06:38:11 +0100

    edits

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
+if (!supabaseURL || !supabaseKey) {
+    throw new Error('Supabase URL and Anon Key are required.');
+  }
+
 export const supabase = createClient(supabaseURL, supabaseKey);
